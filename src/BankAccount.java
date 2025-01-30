394c91641831a5d2dfdd2ecd1629369069b39278
Normano1101 <normanbm123@gmail.com>
2025-01-30 22:39:16 +0000

    Lab2 Assignment

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,18 @@
+//create java class
+public class BankAccount{
+    public static void main(String[] args){
+        double accBalance = 2175.37;
 
+        accBalance -= 302.50;
+        accBalance += 50.03;
+        accBalance -= accBalance/2;
+        accBalance +=20.00;
+        accBalance -= 1;
+        accBalance *= 2;
+        accBalance += 1;
+
+        System.out.print("Bobs Bank Account Balance is $");
+        System.out.printf("%.2f", accBalance);
+
+    }
+}
